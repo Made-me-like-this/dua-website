a444996fae23d94e56638c888d76863b01f3ff71
Abdul Rahman <110932245+Made-me-like-this@users.noreply.github.com>
2025-03-16 17:01:44 +0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('categories/<int:category_id>/', views.category_detail, name='category_detail'),
     path('duas/<int:dua_id>/', views.dua_detail, name='dua_detail'),
     path('learn/', views.learn, name='learn'),
+    path('askquestions/', views.questionaires, name='questions'),
     path('about/', views.about, name='about'),
     path('donate/', views.donate, name='donate'),
 ]
